85640a167d349e0eaad5141935936afb48258485
Genevieve CLoutier <shail1234@hotmail.com>
2024-05-17 08:33:27 -0400

    mise à jour css et facture

--- routerFacture.js
+++ routerFacture.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/facture', function(req, res){
+router.get('/facture', async function(req, res){
+    //const chantier = await db("SELECT * from chantier WHERE id = ")
     res.render("facture")
+})
+
+router.get('/sommaireChantier', function(req, res){ // enlever cette route, elle devrait être dans routerChantier déjà
+    res.render("sommaireChantier") 
 })
 
 module.exports = router;
