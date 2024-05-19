079935829521cc42b677c865b68b643db95e9b12
Martine Desmarais <vitrailvertiti@hotmail.com>
2024-05-18 21:56:06 -0400

    détails

--- routerEmploye.js
+++ routerEmploye.js
@@ -7,15 +7,12 @@ const db = require("./db");
 const d = new Date();
 document.getElementById("date").innerHTML = d;*/
 
-router.get("/:id", async function (req,res) {
-    const employe = await db.execute({sql:"SELECT * FROM utilisateur WHERE id = :id",
-        args: {
-            nom: req.body.nom,
-            id: req.body.numero
-        }
+router.get("/afficher", async function (req,res) {
+    const {rows} = await db.execute({sql:"SELECT * FROM utilisateur WHERE id = :id",
+        args: {id}
     })
     console.log({employe: employe.rows[0]})
-    res.render("employe", {employe: employe.rows[0]});
+    res.render("afficher", rows[0]);
 })
 
 module.exports = router;
