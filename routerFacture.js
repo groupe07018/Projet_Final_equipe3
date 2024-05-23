8ac76b0b63c1018d4a8c9b3c4948f7a3e2e12568
Genevieve CLoutier <shail1234@hotmail.com>
2024-05-23 17:04:49 -0400

    modifications facture 23 mai

--- routerFacture.js
+++ routerFacture.js
@@ -3,13 +3,6 @@ const router = express.Router();
 
 const db = require("./db")
 
-router.get('/facture', async function(req, res){
-    //const chantier = await db.execute ("SELECT * from chantier WHERE id = ")
-    const frais_fixe = await db.execute("SELECT * FROM frais_fixe")
-    //const heure_facturable = await db.execute()
-    res.render("facture", {frais_fixe:frais_fixe.rows})
-})
-
 router.get('/ajoutFraisFixe', function(req, res){
     res.render('ajoutFraisFixe')
 })
@@ -20,7 +13,21 @@ router.post('/frais_fixe', async function(req, res){
         sql: "INSERT INTO frais_fixe(nom_frais_fixe, prix_frais_fixe, unite_frais_fixe) VALUES(:nom_frais_fixe, :prix_frais_fixe, :unite_frais_fixe)",
         args: {nom_frais_fixe, prix_frais_fixe, unite_frais_fixe}
     });
-    res.redirect("facture")
+    res.render("ajoutFraisFixe", 
+        {message: `<div class='alert alert-success alert-dismissible'>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert'></button>Ajout effectué avec succès</div>`})
+})
+
+router.get('/facture', async function(req, res){
+    //const chantier = await db.execute ("SELECT * from chantier WHERE id = ")
+    const frais_fixe = await db.execute("SELECT * FROM frais_fixe")
+    //const heure_facturable = await db.execute()
+    res.render("facture", {frais_fixe:frais_fixe.rows})
+})
+
+router.get('/listeChantier', async function(req, res){// m'assurer que la page sommaire affiche bien les détails du chantier (adresse, et autres)
+    const chantier = await db.execute("SELECT * FROM chantier WHERE statut = 'termine'")
+    res.render("listeChantier", {chantier:chantier.rows})
 })
 
 router.get('/sommaireChantier', function(req, res){ // enlever cette route, elle devrait être dans routerChantier déjà
--- static/script.js
+++ static/script.js
@@ -1,3 +1,7 @@
-function afficherPrixFacture(){
-    //let itemChoisi = document.querySelector(#)
+const itemAFacturer = document.querySelector("#menu_frais_fixe");
+const prixFacture = document.querySelector("#prixFacture");
+
+itemAFacturer.addEventListener("change", afficherPrix);
+function afficherPrix(){
+    prixFacture.innerText = itemAFacturer.value
 }
