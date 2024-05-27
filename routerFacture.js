ffd315ea382fd72ee1cd358ab48a71dc3a7e21d8
Genevieve CLoutier <shail1234@hotmail.com>
2024-05-27 14:51:39 -0400

    update facture

--- routerFacture.js
+++ routerFacture.js
@@ -19,7 +19,7 @@ router.post('/frais_fixe', async function(req, res){
 })
 
 router.get('/facture', async function(req, res){
-    const chantier = await db.execute ("SELECT * from chantier WHERE id=")
+    //const chantier = await db.execute ("SELECT * from chantier WHERE id=")
     const frais_fixe = await db.execute("SELECT * FROM frais_fixe")
     //const heure_facturable = await db.execute()
     res.render("facture", {frais_fixe:frais_fixe.rows})
