06a2ce949b878bccc2da396aad17c7ac6d37107d
Genevieve CLoutier <shail1234@hotmail.com>
2024-05-28 21:40:04 -0400

    commit facture

--- routerFacture.js
+++ routerFacture.js
@@ -20,11 +20,20 @@ router.post('/frais_fixe', async function(req, res){
 
 router.get('/facture', async function(req, res){
     const client= await db.execute("SELECT * from client");
-    const chantierActif = await db.execute ("SELECT * FROM chantier WHERE statut = 'actif'")
-    const chantierInactif = await db.execute ("SELECT * FROM chantier WHERE statut = 'inactif'")
+    const chantierActif = await db.execute (`SELECT ch.*, v.nom_ville, cl.* FROM chantier ch
+    JOIN ville v  
+        ON v.id = ch.id_ville
+    JOIN client cl 
+        ON cl.id = ch.id_client
+     WHERE statut = 'actif'`)
+    const chantierInactif = await db.execute (`SELECT ch.*, v.nom_ville, cl.* FROM chantier ch
+    JOIN ville v  
+        ON v.id = ch.id_ville
+    JOIN client cl 
+        ON cl.id = ch.id_client
+     WHERE statut = 'inactif'`)
     const frais_fixe = await db.execute("SELECT * FROM frais_fixe")
     //const heure_facturable = await db.execute()
-    console.log(chantierInactif)
     res.render("facture", {frais_fixe:frais_fixe.rows, 
         chantierActif:chantierActif.rows, 
         chantierInactif:chantierInactif.rows,
