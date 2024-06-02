0ca252da4e2c304375ac06f959ea27d4b81aff9d
Martine Desmarais <vitrailvertiti@hotmail.com>
2024-06-02 17:06:14 -0400

    mÀj

--- auth/login.js
+++ auth/login.js
@@ -17,6 +17,14 @@ router.get('/routerPatron', async function(req,res) {
     res.render('patron');
 });
 
+router.get('/routerChantier', async function(req,res) {
+    res.render('listeChantier');
+});
+
+router.get('/routerFacture', async function(req,res) {
+    res.render('facture');
+});
+
 router.post('/', async (req,res) => {
     // Avoir les informations du formulaire
     const login = req.body.login;
@@ -60,7 +68,7 @@ router.post('/', async (req,res) => {
     );
     if(correct) {
         addInfo(await createSession(res), {login: user.id}); //p-e ajouter une vérification de session au lieu d'en créer une 
-        if (result.rows[0].profil_utilisateur===1) {
+        if (result.rows[0].profil_utilisateur===1) { //ça ne marche pas ça
             res.redirect("patron");
         }
         else {
